8c9ae4f74ac2478433a840751e1a7bb706b7b521
Mani Kumar <mani.kumar@ibm.com>
2025-10-13 00:48:38 -0400

    hide breadcrumbs users don’t need
    Show ‘Jobs’ for everyone, but only show ‘New Job’ or ‘KYC’ when a contractor is on those pages

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,19 +1,31 @@
 import { Breadcrumb, BreadcrumbItem } from "@carbon/react";
 import { Link, useLocation } from "react-router-dom";
+import { getUser } from "../services/session";
+import "../styles/components/nav.css";
 export default function Nav() {
   const loc = useLocation();
+  const user = getUser();
+  const path = loc.pathname;
+  const isContractor = user?.userType === "contractor";
+  const showNewJob =
+    isContractor && (path.startsWith("/new-job") || path.startsWith("/kyc"));
+  const showKyc = isContractor && path.startsWith("/kyc");
   return (
-    <nav className="container" style={{ marginTop: "4rem" }}>
+    <nav className="container nav-container">
       <Breadcrumb noTrailingSlash>
         <BreadcrumbItem isCurrentPage={loc.pathname === "/jobs"}>
           <Link to="/jobs">Jobs</Link>
         </BreadcrumbItem>
-        <BreadcrumbItem isCurrentPage={loc.pathname === "/new-job"}>
-          <Link to="/new-job">New Job</Link>
-        </BreadcrumbItem>
-        <BreadcrumbItem isCurrentPage={loc.pathname === "/kyc"}>
-          <Link to="/kyc">KYC</Link>
-        </BreadcrumbItem>
+        {(showNewJob || showKyc) && (
+          <BreadcrumbItem isCurrentPage={loc.pathname === "/new-job"}>
+            <Link to="/new-job">New Job</Link>
+          </BreadcrumbItem>
+        )}
+        {showKyc && (
+          <BreadcrumbItem isCurrentPage={loc.pathname === "/kyc"}>
+            <Link to="/kyc">KYC</Link>
+          </BreadcrumbItem>
+        )}
       </Breadcrumb>
     </nav>
   );
