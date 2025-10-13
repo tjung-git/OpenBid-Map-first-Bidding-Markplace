3c57ac5153c8a153f5ac514727b37a0aa5c0c921
Mani Kumar <mani.kumar@ibm.com>
2025-10-13 00:51:24 -0400

    keep jobs protected behind login
    Wrap the app with the new protected layout so only logged-in users reach /jobs, /new-job, etc.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,25 +3,28 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "@carbon/styles/css/styles.css";
 import "./styles/global.scss";
-import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 import KycGate from "./pages/KycGate.jsx";
 import JobList from "./pages/JobList.jsx";
 import NewJob from "./pages/NewJob.jsx";
 import JobDetail from "./pages/JobDetail.jsx";
+import ProtectedApp from "./components/ProtectedApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route element={<App />}>
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<ProtectedApp />}>
           <Route index element={<Navigate to="/jobs" replace />} />
           <Route path="/kyc" element={<KycGate />} />
           <Route path="/jobs" element={<JobList />} />
           <Route path="/jobs/:jobId" element={<JobDetail />} />
           <Route path="/new-job" element={<NewJob />} />
         </Route>
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
