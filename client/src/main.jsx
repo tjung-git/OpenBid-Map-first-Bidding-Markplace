4d35e61ebc8f48b570c218e2464bf2ce9dabc0b9
Mani Kumar <mani.kumar@ibm.com>
2025-10-19 01:36:37 -0400

    Register bidder routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,9 @@ import KycGate from "./pages/KycGate.jsx";
 import JobList from "./pages/JobList.jsx";
 import NewJob from "./pages/NewJob.jsx";
 import JobDetail from "./pages/JobDetail.jsx";
+import JobBid from "./pages/JobBid.jsx";
+import BidDetail from "./pages/BidDetail.jsx";
+import MyBids from "./pages/MyBids.jsx";
 import ProtectedApp from "./components/ProtectedApp.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -22,6 +25,13 @@ createRoot(document.getElementById("root")).render(
           <Route path="/kyc" element={<KycGate />} />
           <Route path="/jobs" element={<JobList />} />
           <Route path="/jobs/:jobId" element={<JobDetail />} />
+          <Route path="/jobs/:jobId/bid" element={<JobBid />} />
+          <Route path="/bids/:jobId" element={<BidDetail />} />
+          <Route path="/jobs/myBids" element={<MyBids />} />
+          <Route
+            path="/jobs/myBids/bidDetails/:jobId"
+            element={<BidDetail />}
+          />
           <Route path="/new-job" element={<NewJob />} />
         </Route>
         <Route path="*" element={<Navigate to="/login" replace />} />
