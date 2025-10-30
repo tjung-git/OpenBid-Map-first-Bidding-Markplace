f1b0a99c7d4fc8b487b306dbd1e7fce1e11c19b0
Tyler Jung <Tyler.Jung@ibm.com>
2025-10-30 12:40:03 -0400

    js -> cjs

--- server/jest.config.cjs
+++ server/jest.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
-  transform: {},
+  transform: {
+    '^.+\\.js$': ['babel-jest', { presets: ['@babel/preset-env'] }],
+  },
+  transformIgnorePatterns: ['node_modules/(?!(supertest|@jest/globals)/)'],
   testMatch: ['**/tests/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', 'testUtils.js'],
   setupFiles: ['<rootDir>/tests/setupenv.js'],
 };
